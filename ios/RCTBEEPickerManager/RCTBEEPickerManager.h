09df4a73502c953594cfbb7f474fb65f0c178185
NoodyYeung <yeungchunyin1996@gmail.com>
2018-01-24 16:22:51 +0800

    Update RCTBEEPickerManager.h
    Fixed for 0.52.1 RN

--- ios/RCTBEEPickerManager/RCTBEEPickerManager.h
+++ ios/RCTBEEPickerManager/RCTBEEPickerManager.h
@@ -7,8 +7,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else // Compatibility for RN version < 0.40
 #import "RCTBridgeModule.h"
-
+#endif
 @interface RCTBEEPickerManager : NSObject<RCTBridgeModule>
 
 @end
